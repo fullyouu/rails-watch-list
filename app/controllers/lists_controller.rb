5762a125f94eca321ff7e56e5b537658ff284a12
VeronikaMueller-Wilmes <vmuellerwilmes@gmail.com>
2021-11-11 14:03:22 +0100

    Finished lists & begining of bookmarks controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    id = params[:id]
+    @list = List.find(id)
+  end
+
+  def new
+    @lists = List.new
+  end
+
+  def create
+    @list = List.new(strong_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:list).permit(:name)
+  end
+end
